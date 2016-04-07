cdaf3573a4037bf378f9ac40c9aef6694a51904f
IbirbyZh <040796@gmail.com>
2016-04-07 15:57:45 +0300

    Initial Commit

---
+++ RayTracer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RayTracer
+//
+//  Created by Шишкин Евгений on 07.04.16.
+//  Copyright © 2016 Шишкин Евгений. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
